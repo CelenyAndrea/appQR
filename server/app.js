4bd01ba5e4f3bfd1763c8d7329912c0ce684bda1
CelenyAndrea <celenyandrea@gmail.com>
2023-10-04 21:37:26 -0500

    ok conec

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ import petRoutes from "./routes/pet.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://app-qr-sigma.vercel.app/",
     credentials: true
 }));
 app.use(morgan("dev"));
