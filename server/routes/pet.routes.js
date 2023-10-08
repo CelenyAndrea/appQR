ee64123899c13528a6955093e61908fbb2689e60
CelenyAndrea <celenyandrea@gmail.com>
2023-10-08 15:47:00 -0500

    cors server

--- server/routes/pet.routes.js
+++ server/routes/pet.routes.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
-import { createPets, deletePets, getPet, getPets, updatePets, optionsPets } from "../controllers/pet.controller.js";
+import { 
+    createPets, 
+    deletePets, 
+    getPet, 
+    getPets, 
+    updatePets, 
+    optionsPets 
+} from "../controllers/pet.controller.js";
 //import { createPetSchema } from "../validatorSchemas/pet.schema.js";
 //import { validateSchema } from "../middlewares/validateSchemas.js";
 
@@ -13,6 +20,6 @@ router.post('/pets',authRequired, createPets)
 //validateSchema(createPetSchema),
 router.put('/pets/:id',authRequired, updatePets)
 router.delete('/pets/:id',authRequired, deletePets)
-router.options('/pets',authRequired, optionsPets)
+router.options('/pets/:id',authRequired, optionsPets)
 
 export default router
