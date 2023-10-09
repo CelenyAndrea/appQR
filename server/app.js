64a95bc592d04053a2f155b68e006e723c9442e5
CelenyAndrea <celenyandrea@gmail.com>
2023-10-08 20:56:02 -0500

    client en server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url'
 
 import authRoutes from "./routes/auth.routes.js";
 import petRoutes from "./routes/pet.routes.js";
@@ -10,18 +12,21 @@ import petRoutes from "./routes/pet.routes.js";
 const app = express();
 app.disable('x-powered-by')
 
-const whiteList = ['http://localhost:5173', 'https://app-qr-sigma.vercel.app/']
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.use(cors({
+app.use(cors())
+
+// const whiteList = ['http://localhost:5173', 'https://app-qr-sigma.vercel.app/']
+
+// app.use(cors({
     
-    //origin: "*",
-    //origin: "http://localhost:5173",
-    origin: whiteList,
-    credentials: true,
-    methods: ['GET','POST','DELETE','UPDATE','PUT'],
-    //allowedHeaders: ['Content-Type', 'Authorization', 'X-Custom-Header'],
-    //optionsSuccessStatus: 200,
-}));
+//     origin: "*",
+//     origin: "http://localhost:5173",
+//     origin: whiteList,
+//     credentials: true,
+//     methods: ['GET','POST','DELETE','UPDATE','PUT'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Custom-Header', 'X-Auth-Token']
+// }));
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -33,5 +38,8 @@ app.use(fileUpload({
 
 app.use("/api", authRoutes);
 app.use("/api", petRoutes);
+
+console.log(__dirname);
+app.use(express.static(join(__dirname, '../client/dist')))
 
 export default app;
