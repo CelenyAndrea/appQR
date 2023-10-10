fb2b68f0d6a76c5950b3b40cd74290eab3567dce
CelenyAndrea <celenyandrea@gmail.com>
2023-10-09 19:11:37 -0500

    env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import app from "./app.js"
 import { connectDB } from "./db.js";
 
+const PORT = process.env.PORT
 // npm run dev
 connectDB();
-app.listen(3001);
-console.log("Ok servidor vercel", 3001);
+app.listen(PORT);
+console.log("Ok servidor render", PORT);
