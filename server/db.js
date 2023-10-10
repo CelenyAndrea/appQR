fd3c8929a20e2b37c8fe715e33da69426f533da4
CelenyAndrea <celenyandrea@gmail.com>
2023-10-09 20:58:53 -0500

    mongoose

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URL)
+        mongoose.connect(config.DB,{ useNewUrlParser: true })
         console.log(">>> Conexion a DB" )
     } catch (err) {
         console.log(err)
